e0a91e233c3197f2b81a3b8a5cc23ab79a28324b
GalSanyi <86233181+GalSanyi@users.noreply.github.com>
2022-10-01 00:30:13 +0200

    hero

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Order from "./views/Order/Order";
 import Hustle from "./views/Hustle/Hustle";
+import Hero from "./views/Hero/Hero";
 export const App = () => {
   return (
     <div>
       <Order/>
       <Hustle/>
+      <Hero/>
     </div>
   );
 };
---
+++ src/components/views/Hero/Hero.js
@@ -0,0 +1,11 @@
+import s from './Hero.module.css';
+
+function Hero() {
+  return (
+    <div className={s.hero__img}>
+
+    </div>
+  )
+}
+
+export default Hero
